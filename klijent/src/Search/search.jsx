998ea35e438fd18c41929eefffc0a4aa2ed6bcbe
stefandobrosavljevic <stefipanter999@gmail.com>
2022-02-13 00:50:39 +0100

    Ispravljena greska kod api-ja za pretragu proizvoda

--- klijent/src/Search/search.jsx
+++ klijent/src/Search/search.jsx
@@ -5,49 +5,27 @@ import "../App.css";
 import ApiClient from "../Global/apiClient";
 import { Alert } from "bootstrap";
 
-
 function Search() {
 	const api = new ApiClient();
 
 	let { parametri } = useParams();
-    const [items, setItems] = useState([]);
+	const [items, setItems] = useState([]);
 
-    useEffect(() => {
-        fetchItems();
-    }, []);
+	useEffect(() => {
+		fetchItems();
+	}, [parametri]);
 
-    const fetchItems = () => {
+	const fetchItems = async () => {
 		console.log(parametri);
 
-		try{
-			let data = api.produkti.pretraziProdukte(parametri);
+		try {
+			const data = await api.produkti.pretraziProdukte(parametri);
 			console.log(data);
-		}catch(e){
+			setItems(data);
+		} catch (e) {
 			alert(e.message);
 		}
-        const data = [
-			{
-				productCode: 1,
-				name: "Bel London poklon set",
-				price: 12000,
-				quantity: 10,
-				description: "deskripcija",
-				image: "https://shop.lilly.rs/media/catalog/product/cache/e9fe89bb0d3d5e05736d64f06cc6558c/5/0/5060693811968_1.jpg",
-				category: "set",
-			},
-			{
-				productCode: 2,
-				name: "Neki proba proizvod",
-				price: 13000,
-				quantity: 10,
-				description: "deskripcija",
-				image: "https://shop.lilly.rs/media/catalog/product/cache/e9fe89bb0d3d5e05736d64f06cc6558c/5/0/5060693811968_1.jpg",
-				category: "proba",
-			},
-		];
-
-		setItems(data);
-    }
+	};
 
 	return (
 		<div className="col-md-8 d-flex flex-column justify-content-center align-items-center">
